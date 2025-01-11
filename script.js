c08ba3be574aff39899fe6c1fc83eda8dbdb8d7a
Shefali <mittal.shefali2000@gmail.com>
2025-01-11 13:39:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+function submitAnswer() {
+    const options = document.getElementsByName('capital');
+    let selectedOption;
+    for (const option of options) {
+        if (option.checked) {
+            selectedOption = option.value;
+            break;
+        }
+    }
+    if (selectedOption) {
+        alert(`You selected: ${selectedOption}`);
+    } else {
+        alert('Please select an option.');
+    }
+}
